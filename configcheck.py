cd42db082c222431e18317e3691d6d6d4eb606a4
debashruti801-droid <debashruti801@gmail.com>
2026-01-29 11:16:59 +0530

    Update configcheck.py

--- configcheck.py
+++ configcheck.py
@@ -1,4 +1,7 @@
 import os
+import json
+import sys
+from lxml import etree
 
 curr_dir = os.getcwd()
 print("current working directory:",curr_dir)
@@ -9,3 +12,9 @@ for dirpath, dirnames, filenames in os.walk(curr_dir):
       arxmlfiles.append(os.path.join(dirpath, file))
 
 print(arxmlfiles)
+CONFIG_PATH = os.path.join(curr_dir, "config_check.json")
+
+def configcheck(file_path, config_path):
+  
+  
+  
